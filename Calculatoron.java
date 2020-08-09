3b7ec66af13cb065b7e71afce53e44d3c4833239
Ajith R B <ajithrb2001@gmail.com>
2020-08-09 19:01:57 +0530

    label is added

--- Calculatoron.java
+++ Calculatoron.java
@@ -1,14 +1,27 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Calculatoron {
-    public Calculatoron() {
-        JFrame jf=new JFrame("CALCULATOR");
-        jf.setLayout(null);
-        jf.setSize(500, 600);
+    public Calculatoron() {//create a constructor
+        JFrame jf=new JFrame("CALCULATOR");//create a frame/window "constructor"
+        jf.setLayout(null);//no need of layout so keep it null
+        jf.setSize(400, 600);//set size
+        jf.setLocation(300, 150);//set location of frame
+        JLabel display=new JLabel("hello !");//label in calculator
+        display.setHorizontalAlignment(SwingConstants.RIGHT);//horizontal allignment
+        display.setBounds(30, 50, 330, 40);
+        display.setFont(new Font("Helvetica Neue", Font.PLAIN, 40));//font size
+      
+        display.setOpaque(true);
 
-
+        jf.add(display);
+        display.setBackground(Color.ORANGE);//background color
         jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//exit after closing the calculator
 
     }
 
